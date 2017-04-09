3dff9ba6cb8b63bf05e26b7f76f40f0473046e1b
Qiang Fan <Qiang.Fan@confirmit.com>
2017-04-09 13:19:39 +0800

    test different user agents

---
+++ downloade_with_diff_userAgent.py
@@ -0,0 +1,31 @@
+import time
+import random
+import urllib.request
+
+url = 'http://forums.devshed.com/php-development-5/limit-loop-value-array-977721.html'
+
+file = open('/home/fanq/Workspace/learn_mysql/user_agent.log', 'r')
+theFirstFiveWordOfUserAgent = ''
+counter = 0
+for line in file:
+    counter = counter + 1
+    userAgent = line.strip()
+    if (theFirstFiveWordOfUserAgent == userAgent[:5]):
+        print(userAgent, 'skiped.')
+        print('\n')
+    else:
+        theFirstFiveWordOfUserAgent = userAgent[:5]
+        headers = {'User-agent': userAgent}
+
+        req = urllib.request.Request(url, headers=headers)
+        try:
+            f = urllib.request.urlopen(req)
+            print(userAgent, 'works well.')
+        except urllib.error.HTTPError as e:
+            print(userAgent, e)
+        # except TimeoutError as e:
+        #     print(userAgent, e)
+        print('\n')
+        waitTime = random.randint(1, 10)
+        time.sleep(waitTime)
+file.close()
