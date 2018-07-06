ba7d83cf0d872a91744dc05b9427c92c43016138
fanqiang <fq267@163.com>
2018-07-06 20:20:58 +0800

    changed

--- download_novels_links.py
+++ download_novels_links.py
@@ -1,5 +1,16 @@
 import BiQuGeDownloader
+from pymongo import MongoClient
+import time
 
-entryUrl = 'http://www.biqugex.com/book_37/'
-hunter = BiQuGeDownloader.BiQuGeDownloader(entryUrl)
-str_of_result = hunter.open_url_return_str(entryUrl)
+conn = MongoClient('localhost', 27017)
+db = conn.novels
+queryArgs = {'status_of_visited ': {'$ne': 1}}
+search_res = db.links_for_books.find(queryArgs).sort("id_of_chapter", -1)
+print(type(search_res))
+for record in search_res:
+    print("id_of_chapter  = %s" % record['id_of_chapter'])
+    updateFilter = {'id_of_chapter': record['id_of_chapter']}
+    updateRes = db.links_for_books.update_one(filter=updateFilter, update={'$set': {"status_of_visited": 1}},
+                                              upsert=True)
+    time.sleep(0.8)
+conn.close()
